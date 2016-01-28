5f4301193983be83cc6d777bc0234c867c7e0f03
Skobaissi <s-kobaissi@kellogg.northwestern.edu>
2016-01-27 21:14:00 -0600

    First draft- 1/27

--- weather.js
+++ weather.js
@@ -2,15 +2,16 @@ $(function() {
   var handleWeatherResponse = function(data) {
     console.log(data);
 
-    // Put your code here to change the "markup" variable. 
+    // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
-    
-    var markup = "The weather report... " + "will be here when I finish my homework.";
-    
-    
+
+      // var markup = {apparentTemperature:"32",cloudCover: "0.92",dewPoint: "25.69",humidity: "0.79", icon: "partly-cloudy-night",nearestStormBearing: "97",nearestStormDistance: "20",ozone: "322.26",precipIntensity: "0",precipProbability: "0",pressure: "1015.62",summary: "Mostly Cloudy",temperature: "31.56",time: "1453677410",visibility: "7.74",windBearing: "124",windSpeed: "1.26"}
+      var markup = "<p>It is " + data.currently.summary + " and " + data.currently.apparentTemperature + " degrees.</p>"
+      var markup = "<p>It is " + data.currently.summary + " and " + data.currently.apparentTemperature + " degrees.</p>"
     // End of your code
 
     $('.weather-report').html(markup);
+    $('.weather-report2').html(markup2);
   }
   $('a.get-the-weather').on('click', function(event) {
     event.preventDefault();

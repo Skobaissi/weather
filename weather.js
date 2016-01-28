6039db16467a73648530605443224b5d9e0fc42e
Skobaissi <s-kobaissi@kellogg.northwestern.edu>
2016-01-28 09:04:08 -0600

    Take 2

--- weather.js
+++ weather.js
@@ -5,9 +5,8 @@ $(function() {
     // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
 
-      // var markup = {apparentTemperature:"32",cloudCover: "0.92",dewPoint: "25.69",humidity: "0.79", icon: "partly-cloudy-night",nearestStormBearing: "97",nearestStormDistance: "20",ozone: "322.26",precipIntensity: "0",precipProbability: "0",pressure: "1015.62",summary: "Mostly Cloudy",temperature: "31.56",time: "1453677410",visibility: "7.74",windBearing: "124",windSpeed: "1.26"}
-      var markup = "<p>It is " + data.currently.summary + " and " + data.currently.apparentTemperature + " degrees.</p>"
-      var markup = "<p>It is " + data.currently.summary + " and " + data.currently.apparentTemperature + " degrees.</p>"
+      var markup = "<p>It is currently " + data.currently.summary + " and " + data.currently.temperature + " degrees.</p><p>It feels like " + data.currently.apparentTemperature + " degrees and the windspeed is " + data.currently.windSpeed + " mph.</p><p>"
+      var markup2 = "<p>It is " + data.currently.icon + " and windspeed is " + data.currently.windSpeed + ".</p>"
     // End of your code
 
     $('.weather-report').html(markup);

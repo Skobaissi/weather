015a4ee954db92c33a097a59b9347d0e6c34c553
Skobaissi <s-kobaissi@kellogg.northwestern.edu>
2016-01-31 17:42:21 -0600

    Sunday take 2

--- weather.js
+++ weather.js
@@ -5,12 +5,16 @@ $(function() {
     // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
 
-      var markup = "<p>It is currently " + data.currently.summary + " and " + data.currently.temperature + " degrees.</p><p>It feels like " + data.currently.apparentTemperature + " degrees and the windspeed is " + data.currently.windSpeed + " mph.</p><p>The 3-day forecast is " + data.daily.data[1].apparentTemperatureMax + " degrees.</p>"
-      // var markup2 = "<p>It is " + data.currently.icon + " and windspeed is " + data.currently.windSpeed + ".</p>"
+      var markup = "<p>In <strong>" + data.timezone + "</strong>, it is currently: <ul><li> " + data.currently.summary + " and " + data.currently.temperature + " degrees" +
+      "<li>It feels like " + data.currently.apparentTemperature + " degrees and the windspeed is " + data.currently.windSpeed + " mph</ul></p>" +
+      "<p>The 3-day forecast is: <ul><li>Tomorrow: " + data.daily.data[1].summary + " The high will be " + data.daily.data[1].temperatureMax + " degrees and the low will be " + data.daily.data[1].temperatureMin + " degrees" +
+      "<li>The next day: " + data.daily.data[2].summary + " The high will be " + data.daily.data[2].temperatureMax + " degrees and the low will be " + data.daily.data[2].temperatureMin + " degrees" +
+      "<li>The day after that: " + data.daily.data[3].summary + " The high will be " + data.daily.data[3].temperatureMax + " degrees and the low will be " + data.daily.data[3].temperatureMin + " degrees</ul></p>" +
+      "<p>Here's a summary of the next 7 days: " + data.daily.summary + " </p>"
+
     // End of your code
 
     $('.weather-report').html(markup);
-    $('.weather-report2').html(markup2);
   }
   $('a.get-the-weather').on('click', function(event) {
     event.preventDefault();
